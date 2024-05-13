99639041a9544aaa6cb6eee077375e207d70f6cf
Md. Tanvir Hassan <127887552+ReTr0-MaKeR@users.noreply.github.com>
2024-05-13 23:30:30 +0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,34 +5,33 @@ Daffodil International University
 Date of development: 10th May 2024
 */
 
-#include <Arduino.h>
-#include <WiFi.h>
-#include "time.h"
-#include <ESP_Google_Sheet_Client.h>
+#include <Arduino.h>    //Library for Arduino 
+#include <WiFi.h>       // Library for Wifi uses 
+#include "time.h"        // Library for using time function
+#include <ESP_Google_Sheet_Client.h>  //Libray for Google sheet
 
-#include <GS_SDHelper.h>
+#include <GS_SDHelper.h>  // Library for google sheet data handling
 
-#define WIFI_SSID "Your_WiFi_SSID"
-#define WIFI_PASSWORD "Your_WiFi_Password"
+#define WIFI_SSID "Test-Project"
+#define WIFI_PASSWORD "Test-Project"
 
-#define PROJECT_ID "Your_Project_ID"
-#define CLIENT_EMAIL "Your_Client_Email"
-
-const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY\n-----END PRIVATE KEY-----\n";
-
-const char spreadsheetId[] = "Your_Spreadsheet_ID";
+#define PROJECT_ID "Your_Project_ID" // Taken from personal google script account
+#define CLIENT_EMAIL "Your_Client_Email" // Same as before 
+const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY\n-----END PRIVATE KEY-----\n";//same as before 
+const char spreadsheetId[] = "10tDBylsn3qXCu7t9EUrwYP8iNWKHPQHslTZERy1MYyw"; //taken from google sheet !must be public 
 
 unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
+unsigned long timerDelay = 30000; // equivalen to 30 second ~used to delay the data sending process
 
 void tokenStatusCallback(TokenInfo info);
 
-const char* ntpServer = "pool.ntp.org";
+const char* ntpServer = "pool.ntp.org";// taken from google script 
 
 unsigned long epochTime;
 
-String getTime() {
-    time_t now;
+String getTime()  // this whole function was used for fetching the realtime date and time .
+{
+    time_t now;// for fetching the  real time 
     struct tm timeinfo;
     time(&now);
     gmtime_r(&now, &timeinfo);
@@ -42,89 +41,100 @@ String getTime() {
         timeinfo.tm_mday++; 
     }
     char formattedTime[50];
-    strftime(formattedTime, sizeof(formattedTime), "%Y-%m-%d %H:%M:%S", &timeinfo);
+    strftime(formattedTime, sizeof(formattedTime), "%Y-%m-%d %H:%M:%S", &timeinfo);// to get this following format 5/13/2024 18:58:00
     return String(formattedTime);
 }
 
-#define gasPin 35
-#define lightPin 34
-#define button1Pin 25
-#define button2Pin 26
-#define led1Pin 32
+#define gasPin 35 //  for the gas sensor, used D0 pin not the A0 pin
+#define lightPin 34 // for the light sesnor 
+#define button1Pin 25// used to control the light or led
+#define button2Pin 26// same as before 
+#define led1Pin 32  // used for led 
 #define led2Pin 33
 
-bool led1State = false;
-bool led2State = false;
+bool led1State = false;  // forcefully turing off both leds
+bool led2State = false;  // forcefully turing off both leds
 
-bool button1State = false;
-bool button2State = false;
+bool button1State = false;  // forcefully turing off both buttons or switches 
+bool button2State = false;   // forcefully turing off both buttons or switches 
 
-void setup(){
-    Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200); // used to initate the serial monitor for debuging purpose 
     Serial.println();
 
-    pinMode(gasPin,INPUT);
+    pinMode(gasPin,INPUT);  // used to declare the pins , if they are input or output type 
     pinMode(lightPin,INPUT);
-    pinMode(button1Pin, INPUT_PULLUP);
-    pinMode(button2Pin, INPUT_PULLUP);
+    pinMode(button1Pin, INPUT_PULLUP); // we used pull up to constantly propting the processor that button is at HIGH or True state
+    pinMode(button2Pin, INPUT_PULLUP); // we used pull up to constantly propting the processor that button is at HIGH or True state
     pinMode(led1Pin, OUTPUT);
     pinMode(led2Pin, OUTPUT);
 
-    configTime(0, 0, "time.google.com");
+    configTime(0, 0, "time.google.com"); // just ensuring that time is correct with bd time 
     const char *tz = "BDT-6";
     setenv("TZ", tz, 1);
     tzset();
 
-    GSheet.printf("ESP Google Sheet Client v%s\n\n", ESP_GOOGLE_SHEET_CLIENT_VERSION);
+    GSheet.printf("ESP Google Sheet Client v%s\n\n", ESP_GOOGLE_SHEET_CLIENT_VERSION); // prompting the google sheet that we will use it from now on 
 
     WiFi.setAutoReconnect(true);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // used for connecting to the wifi network 
 
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting to Wi-Fi"); 
+    //********************************************************************************************************************
+    while (WiFi.status() != WL_CONNECTED) {  //  used for debugging , to know if wifi is connected or not 
       Serial.print(".");
-      delay(1000);
+      delay(1000); //resting for 1 second 
     }
     Serial.println();
     Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.localIP()); // for debugging , to know the ip adress 
     Serial.println();
-
+ //************************************************************************************************************************
     GSheet.setTokenCallback(tokenStatusCallback);
     GSheet.setPrerefreshSeconds(10 * 60);
-    GSheet.begin(CLIENT_EMAIL, PROJECT_ID, PRIVATE_KEY);
+    GSheet.begin(CLIENT_EMAIL, PROJECT_ID, PRIVATE_KEY);// google will verify the user from those credentials 
 }
 
 void loop(){
-    bool ready = GSheet.ready();
+    bool ready = GSheet.ready();// ask google sheet it it is ready or not to take the data 
 
-    if (ready && millis() - lastTime > timerDelay){
-        lastTime = millis();
-        FirebaseJson response;
-        String currentTime = getTime();
-        int gasValue = digitalRead(gasPin);
+    if (ready && millis() - lastTime > timerDelay)// if it is ready and 30 second delay is over the following work will be done 
+    {
+    //***********************************************************************************************
+        lastTime = millis();// keep track of the time 
+        
+        FirebaseJson response;   // prompting the google server 
+        String currentTime = getTime(); getting the time 
+
+        
+        int gasValue = digitalRead(gasPin); // reading the gas sensor 
         String gasStatus;
         if (gasValue == 1) {
             gasStatus = "Fresh";
         } else {
             gasStatus = "Harmful";
         }
-        int lightValue = digitalRead(lightPin);
+        
+        int lightValue = digitalRead(lightPin);// reading light sensor 
         String lightStatus;
         if (lightValue == 0) {
             lightStatus = "light";
         } else {
             lightStatus = "dark";
         }
-        String led1Status = led1State ? "On" : "Off";
-        String led2Status = led2State ? "On" : "Off";
+        String led1Status = led1State ? "On" : "Off";// monitoring the led status if they are one 
+        String led2Status = led2State ? "On" : "Off";// monitoring the led status if they are one
+        //*************************************************by this time we got all the 4 data we need to send *************************************
         FirebaseJson valueRange;
-        valueRange.add("majorDimension", "COLUMNS");
-        valueRange.set("values/[0]/[0]", currentTime);
-        valueRange.set("values/[1]/[0]", gasStatus);
+        
+        valueRange.add("majorDimension", "COLUMNS"); // setting up the coloumns 
+        valueRange.set("values/[0]/[0]", currentTime);// sending time and date to the 1st coloumn 
+        valueRange.set("values/[1]/[0]", gasStatus);  // sending gas data to 2nd coloumn 
         valueRange.set("values/[2]/[0]", lightStatus);
         valueRange.set("values/[3]/[0]", led1Status);
         valueRange.set("values/[4]/[0]", led2Status);
+        //--------------------------------------------------------------------------------------------- for debugging*--------------- 
         bool success = GSheet.values.append(&response, spreadsheetId, "Sheet1!A1", &valueRange);
         if (success){
             response.toString(Serial, true);
@@ -135,9 +145,11 @@ void loop(){
         }
         Serial.println();
         Serial.println(ESP.getFreeHeap());
+        /--------------------------------------------------------------------------------------------------------------------------------
     }
-
-    if (digitalRead(button1Pin) == LOW && !button1State) {
+//***************************************************************************************************************
+    if (digitalRead(button1Pin) == LOW && !button1State) // creating software latch and controlling led 
+    {
         led1State = !led1State;
         digitalWrite(led1Pin, led1State ? HIGH : LOW);
         button1State = true;
